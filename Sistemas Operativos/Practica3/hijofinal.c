d94e5a715f43d8a35387aa7edc174c9803fb4714
Luis Coto <81122669+luiscoto@users.noreply.github.com>
2023-01-29 20:31:51 -0600

    Delete Sistemas Operativos directory

--- Sistemas Operativos/Practica3/hijofinal.c
+++
@@ -1,9 +0,0 @@
-#include <windows.h>
-#include <stdio.h>
-
-int main()
-    {
-	   printf("\n\t\t\t Nivel 4");
-	   printf("\n\t\t\t --->Soy el hijo, PID: %d\n", GetCurrentProcessId());
-	   return 0;
-     }
